fa12e2f01d1abd79bf94e560a0e2ac430789fe3a
Asmit Raj <asmitraj9472@gmail.com>
2026-09-15 15:42:36 +0530

    Time: 0 ms (100%), Space: 8.8 MB (92.46%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,23 +3,42 @@ public:
     int lengthOfLastWord(string s) {
         int count = 0;
         int prevCount = 0;
-        for(int i = 0;i < s.size();i++)
-        {
-            if(count == 0 && s[i] != ' ')
-            {
-                prevCount = 1;
-                count++;
-            }
+        // for(int i = 0;i < s.size();i++)
+        // {
+        //     if(count == 0 && s[i] != ' ')
+        //     {
+        //         prevCount = 1;
+        //         count++;
+        //     }
 
-            else if(s[i] != (' '))
+        //     else if(s[i] != (' '))
+        //     {
+        //         count++;
+        //         prevCount++;
+        //     }
+        //     else if(s[i] == ' ')
+        //     {
+        //         count = 0;
+        //     }
+        // }
+        int j = s.size()-1;
+        while(j >= 0)
+        {
+            if(count == 0 && s[j] == ' ')
+            {
+                j--;
+                continue;
+            }
+            else if(s[j] == ' ')
+            {
+                return prevCount;
+            }
+            else
             {
                 count++;
                 prevCount++;
             }
-            else if(s[i] == ' ')
-            {
-                count = 0;
-            }
+            j--;
         }
         return prevCount;
         
